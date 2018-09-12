711000fe37b926c582503fe882c948470d76e29b
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2018-09-12 22:25:33 +0600

    [feature] board creation mechanism initiated

--- test/TicTacToe/ModelSpec.hs
+++ test/TicTacToe/ModelSpec.hs
@@ -2,7 +2,7 @@ module TicTacToe.ModelSpec where
 
 import Test.Hspec (shouldBe, shouldNotBe, Spec, it, describe)
 
-import TicTacToe.Model (Move(X, Y), Position(..), Board(..), moveAtPosition, positionOfBoard)
+import TicTacToe.Model (Move(X, Y), Position(..), Board(..), moveAtPosition, positionOfBoard, getBoardOfLength)
 
 spec :: Spec
 spec = describe "Model" $ do
@@ -25,3 +25,16 @@ spec = describe "Model" $ do
   describe "positionOfBoard" $ do
     it "returns the respective position at board" $ do
        positionOfBoard (Board ([[Position $ Nothing]])) 0 0 `shouldBe` (Position $ Nothing)
+
+  describe "getBoardOfLength" $ do
+    it "returns a board of 4 positions with nothing in them" $ do
+       getBoardOfLength 2 `shouldBe` Board [[Position $ Nothing, Position $ Nothing], [Position $ Nothing, Position $ Nothing]]
+
+    it "returns a board of 9 positions with nothing in them" $ do
+       getBoardOfLength 3 `shouldBe` Board [[Position $ Nothing, Position $ Nothing, Position $ Nothing],
+                                              [Position $ Nothing, Position $ Nothing, Position $ Nothing],
+                                                [Position $ Nothing, Position $ Nothing, Position $ Nothing]]
+
+    it "returns a board of 0 positions" $ do
+       getBoardOfLength 0 `shouldBe` Board []
+
